073152817c03786cb4ca160ef87924639fc5dd52
Dawid <dawidgawronskicontact@gmail.com>
2023-03-07 21:59:37 +0000

    .

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# doin-tic-tac-toe
+Assignment
+
+Set up your project with HTML, CSS and Javascript files and get the Git repo all set up.
+
+Store the gameboard as an array inside of a Gameboard object
+
+Your players are also going to be stored in objects.
+You’re probably going to want an object to control the flow of the game itself.
+
+Try tucking everything away inside of a module or factory.
+
+Write a JavaScript function that will render the contents of the gameboard array to the webpage.
+
+Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
+
+Each little piece of functionality should be able to fit in the game, player or gameboard objects.
+
+Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
+
+Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
--- app.js
+++ app.js
@@ -1,48 +1,158 @@
-(function () {
-  let Game = {
-    gameboardArr: [null, null, null, null, null, null, null, null, null],
-    fieldsRenderd: false,
-    option: "X",
-    _init: function () {
-      this._cacheDOM();
-      this._renderGameboard();
-      this.bindEvents();
-      this._render();
-    },
-    _cacheDOM: function () {
-      this.gameModule = document.querySelector("#gameModule");
-      this.gameboard = this.gameModule.querySelector("#gameboard");
-    },
-    _renderGameboard: function () {
-      if (!this.fieldsRenderd) {
-        this.gameboardArr.forEach(
-          (field) =>
-            (gameboard.innerHTML += `<li class="field" data-selected="false"></li>`)
-        );
-        this.fieldsRenderd = true;
-      }
-    },
-    _render: function () {
-      for (let i = 0; i < this.gameboardArr.length; i++) {
-        if (this.gameboardArr[i] !== null) {
-          this.fields[i].innerHTML = `${this.gameboardArr[i]}`;
-        }
-      }
-    },
-    bindEvents: function () {
-      this.fields = Array.from(this.gameModule.querySelectorAll(".field"));
-      this.fields.forEach((field) =>
-        field.addEventListener("click", this.addOption.bind(this))
-      );
-    },
-    addOption: function (e) {
-      if (this.gameboardArr[this.fields.indexOf(e.target)] === null) {
-        this.gameboardArr[this.fields.indexOf(e.target)] = this.option;
-        console.log(this.gameboardArr);
-        this.option === "X" ? (this.option = "O") : (this.option = "X");
-        this._render();
-      }
-    },
+const Game = (() => {
+  let gameboardArr = [
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+    { mark: " ", active: true },
+  ];
+
+  let gameboard;
+  let fields;
+  let mark = "X";
+  let players = [];
+
+  const playerFactory = (name, mark) => {
+    return { name, mark };
   };
-  Game._init();
+
+  const addPlayer = (name, mark) => {
+    players.push(playerFactory(name, mark));
+    checkPlayers();
+  };
+
+  const checkPlayers = () => {
+    //FIRST PLAYER
+    if (players[0].mark !== "X" && players[0].mark !== "O") {
+      players[0].mark = "X";
+    }
+    //SECOND PLAYER
+    if (players[1] && players[0].mark === "X") {
+      players[1].mark = "O";
+    }
+    if (players[1] && players[0].mark === "O") {
+      players[1].mark = "X";
+    }
+    if (players[1] && players[0].name === players[1].name) {
+      players.pop();
+    }
+    //IF THERE ARE TWO PLAYERS
+    if (players.length > 2) {
+      players.pop();
+    }
+
+    console.table(players);
+  };
+
+  const startGame = () => {
+    render();
+    cacheDOM();
+    bindEvents();
+    console.table(gameboardArr);
+  };
+
+  const cacheDOM = () => {
+    gameboard = document.querySelector("#gameboard");
+    fields = Array.from(document.querySelectorAll(".field"));
+  };
+
+  const render = () => {
+    cacheDOM();
+    gameboard.innerHTML = "";
+    gameboardArr.forEach((field) => {
+      gameboard.innerHTML += `<li class="field"></li>`;
+    });
+  };
+
+  const bindEvents = () => {
+    fields.forEach((field) => field.addEventListener("click", addMark));
+  };
+
+  const addMark = (e) => {
+    const type = typeof e;
+    type == "number" ? (e = e) : (e = fields.indexOf(e.target));
+    let index = e;
+    if (gameboardArr[index].active === true) {
+      gameboardArr[index].mark = mark;
+      gameboardArr[index].active = false;
+      changePlayer();
+
+      console.clear();
+      console.table(gameboardArr);
+      console.table(players);
+      checkWinner(gameboardArr);
+    }
+  };
+
+  const changePlayer = () => {
+    mark === "X" ? (mark = "O") : (mark = "X");
+  };
+
+  const checkWinner = (arr) => {
+    // rows
+    if (
+      arr[0].mark === arr[1].mark &&
+      arr[1].mark === arr[2].mark &&
+      arr[1].active === false
+    ) {
+      console.log("Win!");
+    }
+    if (
+      arr[3].mark === arr[4].mark &&
+      arr[4].mark === arr[5].mark &&
+      arr[4].active === false
+    ) {
+      console.log("Win!");
+    }
+    if (
+      arr[6].mark === arr[7].mark &&
+      arr[7].mark === arr[8].mark &&
+      arr[7].active === false
+    ) {
+      console.log("Win!");
+    }
+    // columns
+    if (
+      arr[0].mark === arr[3].mark &&
+      arr[3].mark === arr[6].mark &&
+      arr[3].active === false
+    ) {
+      console.log("Win!");
+    }
+    if (
+      arr[1].mark === arr[4].mark &&
+      arr[4].mark === arr[7].mark &&
+      arr[4].active === false
+    ) {
+      console.log("Win!");
+    }
+    if (
+      arr[2].mark === arr[5].mark &&
+      arr[5].mark === arr[8].mark &&
+      arr[5].active === false
+    ) {
+      console.log("Win!");
+    }
+    // diagonal
+    if (
+      arr[0].mark === arr[4].mark &&
+      arr[4].mark === arr[8].mark &&
+      arr[4].active === false
+    ) {
+      console.log("Win!");
+    }
+    if (
+      arr[3].mark === arr[5].mark &&
+      arr[5].mark === arr[6].mark &&
+      arr[5].active === false
+    ) {
+      console.log("Win!");
+    }
+  };
+
+  return { startGame, addMark, addPlayer };
 })();
